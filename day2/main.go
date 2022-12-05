bfe386160497fe9baf62b4fd04b97b0a4653c338
godwinrob <rob@robgodwin.com>
2022-12-05 13:10:21 -0500

    completed day 2

--- day2/main.go
+++ day2/main.go
@@ -7,27 +7,32 @@ import (
 )
 
 var (
-	totalScore int
-	roundScore int
-	elfPlay    byte
-	myPlay     byte
+	totalScore1 int
+	totalScore2 int
+	roundScore  int
+	elfPlay     byte
+	myPlay      byte
 )
 
 func main() {
+
+	total1 := part1()
+	total2 := part2()
+
+	log.Printf("Total part 1 score: %v", total1)
+	log.Printf("Total part 2 score: %v", total2)
+
+}
+
+func part1() int {
 	input, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatalf("Error opening input file: %v", err)
 	}
 	defer input.Close()
-
-	part1(input)
-
-}
-
-func part1(input *os.File) {
-
 	fileScanner := bufio.NewScanner(input)
 	fileScanner.Split(bufio.ScanLines)
+
 	for fileScanner.Scan() {
 		elfPlay = fileScanner.Text()[0]
 		myPlay = fileScanner.Text()[2]
@@ -66,12 +71,62 @@ func part1(input *os.File) {
 				roundScore = 6
 			}
 		}
-		totalScore += roundScore
+		totalScore1 += roundScore
 		roundScore = 0
 	}
-	log.Printf("Total part 1 score: %v", totalScore)
+	return totalScore1
 }
 
-func part2(input *os.File) {
+func part2() int {
+	input, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("Error opening input file: %v", err)
+	}
+	defer input.Close()
+	fileScanner := bufio.NewScanner(input)
+	fileScanner.Split(bufio.ScanLines)
 
+	fileScanner.Split(bufio.ScanLines)
+	for fileScanner.Scan() {
+		elfPlay = fileScanner.Text()[0]
+		myPlay = fileScanner.Text()[2]
+
+		if elfPlay == 'A' { // Rock
+			if myPlay == 'X' {
+				// Lose Scissors with 3 point
+				roundScore = 3
+			} else if myPlay == 'Y' {
+				// Draw Rock with 1 points
+				roundScore = 4
+			} else {
+				// Win Paper with 2 points
+				roundScore = 8
+			}
+		} else if elfPlay == 'B' { // Paper
+			if myPlay == 'X' {
+				// Loss Rock with 1 point
+				roundScore = 1
+			} else if myPlay == 'Y' {
+				// Draw Paper with 2 points
+				roundScore = 5
+			} else {
+				// Win Scissors with 3 points
+				roundScore = 9
+			}
+		} else if elfPlay == 'C' { // Scissors
+			if myPlay == 'X' {
+				// Lose paper with 2 point
+				roundScore = 2
+			} else if myPlay == 'Y' {
+				// Draw Scissors with 3 point
+				roundScore = 6
+			} else {
+				// Win Rock with 1 points
+				roundScore = 7
+			}
+		}
+		totalScore2 += roundScore
+		roundScore = 0
+	}
+	return totalScore2
 }
